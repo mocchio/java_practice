c49b46cebece935c80e06662076a7a5ce10ae109
mocchio <skmt.rome@gmail.com>
2023-05-16 12:51:55 +0900

    ラムダ式の記述

--- practice3/Main.java
+++ practice3/Main.java
@@ -2,8 +2,14 @@ public class Main {
   public static void main(String[] args) {
     FuncList funclist = new FuncList();
     // 関数の代入
-    Func1 f1 = FuncList::isOdd;
-    Func2 f2 = funclist::passCheck;
+    // Func1 f1 = FuncList::isOdd;
+    // Func2 f2 = funclist::passCheck;
+
+    // ラムダ式での関数の代入
+    Func1 f1 = x -> x % 2 == 1;
+    Func2 f2 = (point, name) -> {
+      return name + "さんは" + (point > 65 ? "合格" : "不合格");
+    };
     // 関数の呼び出し
     System.out.println(f1.call(15));
     System.out.println(f2.call(66, "ジョン"));
