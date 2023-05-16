1f09ca04de0a4e693571fec706138e051853c1c3
mocchio <skmt.rome@gmail.com>
2023-05-16 12:56:43 +0900

    標準関数インターフェースの利用

--- practice3/Main.java
+++ practice3/Main.java
@@ -1,3 +1,5 @@
+import java.util.function.*;
+
 public class Main {
   public static void main(String[] args) {
     FuncList funclist = new FuncList();
@@ -6,12 +8,12 @@ public class Main {
     // Func2 f2 = funclist::passCheck;
 
     // ラムダ式での関数の代入
-    Func1 f1 = x -> x % 2 == 1;
+    IntPredicate f1 = x -> x % 2 == 1;
     Func2 f2 = (point, name) -> {
       return name + "さんは" + (point > 65 ? "合格" : "不合格");
     };
     // 関数の呼び出し
-    System.out.println(f1.call(15));
+    System.out.println(f1.test(15));
     System.out.println(f2.call(66, "ジョン"));
   }
 }
