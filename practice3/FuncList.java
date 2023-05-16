2ed35602ea5a0b19f1c52a0fe2ec3a034c94d674
mocchio <skmt.rome@gmail.com>
2023-05-16 12:15:24 +0900

    メソッドの定義

---
+++ practice3/FuncList.java
@@ -0,0 +1,9 @@
+public class FuncList {
+  public static boolean isOdd(int x) {
+    return (x % 2 == 1);
+  }
+
+  public String passCheck(int point, String name) {
+    return name + "さんは" + (point > 65 ? "合格" : "不合格"); // 三項条件演算子
+  }
+}
