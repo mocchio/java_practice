4f3188ff3c109e4c8fab1443895eced59b7929a4
mocchio <skmt.rome@gmail.com>
2023-05-17 18:11:32 +0900

    FileInputStreamクラスとFileOutputStreamクラスを使ったプログラム

---
+++ practice6/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    String inFile = args[0];
+    String outFile = args[1];
+
+    FileInputStream fis = new FileInputStream(inFile);
+    FileOutputStream fos = new FileOutputStream(outFile);
+
+    int i = fis.read();
+    while (i != -1) {
+      fos.write(i);
+      i = fis.read(); // 次のバイトの値を読み込み
+    }
+
+    fos.flush();
+    fos.close();
+    fis.close();
+  }
+}
