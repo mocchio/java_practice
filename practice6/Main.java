1e7eb5aca700121497972b883c7f891dc379736f
mocchio <skmt.rome@gmail.com>
2023-05-18 11:50:32 +0900

    GZIPOutputStreamの利用

--- practice6/Main.java
+++ practice6/Main.java
@@ -1,21 +1,44 @@
 import java.io.*;
+import java.util.zip.GZIPOutputStream;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    String inFile = args[0];
-    String outFile = args[1];
+  public static void main(String[] args) {
+    // // FileInputStreamとFileOutputStreamの利用
+    // String inFile = args[0];
+    // String outFile = args[1];
 
-    FileInputStream fis = new FileInputStream(inFile);
-    FileOutputStream fos = new FileOutputStream(outFile);
+    // FileInputStream fis = new FileInputStream(inFile);
+    // FileOutputStream fos = new FileOutputStream(outFile);
 
-    int i = fis.read();
-    while (i != -1) {
-      fos.write(i);
-      i = fis.read(); // 次のバイトの値を読み込み
+    // int i = fis.read();
+    // while (i != -1) {
+    //   fos.write(i);
+    //   i = fis.read(); // 次のバイトの値を読み込み
+    // }
+
+    // fos.flush();
+    // fos.close();
+    // fis.close();
+
+    // GZIPOutputStreamの利用
+    String inFile = "2022/5/18 Javaテスト\n以上";
+    String outFile = "/User/java_projects/java_test.txt";
+    
+    // 例外処理
+    try (
+      FileInputStream fis = new FileInputStream(inFile);
+      FileOutputStream fos = new FileOutputStream(outFile);
+      BufferedOutputStream bos = new BufferedOutputStream(fos);
+      GZIPOutputStream gzos = new GZIPOutputStream(bos);
+    ) {
+      int i = fis.read();
+      while (i != -1) {
+        gzos.write(i);
+        i = fis.read();
+      }
+      gzos.flush();
+    } catch (IOException e) {
+      System.out.println("ファイル処理に失敗しました");
     }
-
-    fos.flush();
-    fos.close();
-    fis.close();
   }
 }
