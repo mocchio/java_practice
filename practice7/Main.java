1a54d21d1354512fc4a60851ab5ed6c6797272a0
mocchio <skmt.rome@gmail.com>
2023-05-18 13:34:24 +0900

    ResourceBundleを利用したプロパティファイルの読み取り

--- practice7/Main.java
+++ practice7/Main.java
@@ -3,10 +3,16 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws Exception {
-    Reader fr = new FileReader("pref.properties");
-    Properties p = new Properties();
-    p.load(fr);
-    System.out.println(p.getProperty("aichi.capital") + ":" + p.getProperty("aichi.food"));
-    fr.close();
+    // // プロパティファイルの読み取り
+    // Reader fr = new FileReader("pref.properties");
+    // Properties p = new Properties();
+    // p.load(fr);
+    // System.out.println(p.getProperty("aichi.capital") + ":" + p.getProperty("aichi.food"));
+    // fr.close();
+
+    // ResourceBundleを利用したプロパティファイルの読み取り
+    // クラスパスを起点に読み取り
+    ResourceBundle rb = ResourceBundle.getBundle("pref"); // 「.properties」は記述しない
+    System.out.println(rb.getString("aichi.capital") + ":" + rb.getString("aichi.food"));
   }
 }
