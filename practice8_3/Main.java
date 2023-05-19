e9b0b54885addbe79166f9fcb943559f7b24f7a7
mocchio <skmt.rome@gmail.com>
2023-05-19 22:43:36 +0900

    WebAPIの操作

---
+++ practice8_3/Main.java
@@ -0,0 +1,36 @@
+import java.net.URI;
+import java.net.http.*;
+import java.net.http.HttpClient.*;
+import com.fasterxml.jackson.databind.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    String url = "https://api.github.com/users/miyabilink";
+
+    // HttpClientインスタンスの作成 
+    HttpClient client = HttpClient.newBuilder()
+      .version(Version.HTTP_1_1)
+      .followRedirects(Redirect.NORMAL)
+      .build();
+
+    // HttpRequestインスタンスの作成
+    HttpRequest request = HttpRequest.newBuilder()
+      .uri(URI.create(url))
+      .header("Accept", "application/vnd.github.v3+json")
+      .GET()
+      .build();
+    
+    // 同期方式のリクエスト送信
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    String body = response.body();
+    int status = response.statusCode();
+    if (status == 200) {
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode root = mapper.readTree(body);
+      String blog = root.get("blog").textValue();
+      System.out.println(blog);
+    } else {
+      System.out.println("ERROR:" + status);
+    }
+  }
+}
