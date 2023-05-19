d45aa2d4ad18da49d14dc058d5a53d696262320b
mocchio <skmt.rome@gmail.com>
2023-05-19 16:07:42 +0900

    URLクラスを用いた画像ファイルの取得

---
+++ practice8/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    // URLクラスを用いた画像ファイルの取得
+    URL url = new URL("https://dokojava.jp/favicon.ico");
+    InputStream is = url.openStream();
+    OutputStream os = new FileOutputStream("dj.ico");
+    int i = is.read();
+    while (i != -1) {
+      os.write((byte)i);
+      i = is.read();
+    }
+    is.close();
+    os.flush();
+    os.close();
+  }
+}
